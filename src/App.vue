c2bcc976f6929bd74067316570126a86a146b336
Marin <mrbumbac@gmail.com>
2021-01-12 20:46:36 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+<script>
+import Cart from '@/components/Cart'
+
+export default {
+  name: 'App',
+  components: {
+    Cart
+  },
+  data: () => ({
+    snackbar: false
+  }),
+  methods: {
+    showSnack () {
+      this.snackbar = true
+    }
+  }
+}
+</script>
+
+<template>
+  <div id="app">
+    <v-app>
+      <v-app-bar
+        app
+        color="deep-purple accent-4"
+        dark
+        height="60"
+        class="wrapper"
+      >
+        <v-app-bar-nav-icon to="/">
+          <v-icon large>
+            mdi-laptop
+          </v-icon>
+        </v-app-bar-nav-icon>
+
+        <span class="title ml-3 mr-5">Configurator <span class="font-weight-light">PC</span></span>
+
+        <v-spacer></v-spacer>
+
+        <v-btn text to="/">
+          Products
+        </v-btn>
+
+        <v-btn text class="mx-3" to="/orders">
+          Orders
+        </v-btn>
+
+        <Cart :showSnack = "showSnack"/>
+      </v-app-bar>
+
+      <v-content class="router-view">
+          <router-view></router-view>
+        <v-snackbar
+          v-model="snackbar"
+          color="green"
+          bottom
+          right
+        >
+          Order is processed!
+          <v-btn
+            text
+            @click="snackbar = false"
+          >
+            Close
+          </v-btn>
+        </v-snackbar>
+      </v-content>
+    </v-app>
+  </div>
+</template>
+
+<style>
+  .wrapper, .router-view {
+    padding-left: 11%!important;
+    padding-right: 11%!important;
+  }
+  html {
+    overflow: hidden !important;
+  }
+</style>
---
+++ src/views/Products.vue
@@ -0,0 +1,197 @@
+<script>
+export default {
+  name: 'Products',
+  data () {
+    return {
+      selectedComponent: 'motherboards',
+      showFilters: false,
+      list: [
+        { text: 'Motherboards', icon: 'mdi-chart-tree', value: 'motherboards' },
+        { text: 'Processors', icon: 'mdi-memory', value: 'processors' },
+        { text: 'Ram', icon: 'mdi-alpha-r-box', value: 'ram' },
+        { text: 'Rom', icon: 'mdi-harddisk', value: 'rom' },
+        { text: 'Videocards', icon: 'mdi-rectangle', value: 'videocards' },
+        { text: 'Monitors', icon: 'mdi-monitor', value: 'monitors' },
+        { text: 'Mouses', icon: 'mdi-rodent', value: 'mouses' },
+        { text: 'Keyboards', icon: 'mdi-keyboard', value: 'keyboards' }
+      ],
+      items: {},
+      filters: {}
+    }
+  },
+  computed: {
+    cartComponents () {
+      return this.$store.getters.getCart
+    },
+    filteredItems () {
+      const items = Object.assign({}, this.items)
+      const processorIndex = this.cartComponents.findIndex(item => item.type === 'processors')
+      const motherboardIndex = this.cartComponents.findIndex(item => item.type === 'motherboards')
+      const ramIndex = this.cartComponents.findIndex(item => item.type === 'ram')
+
+      if (processorIndex !== -1) {
+        const processor = this.cartComponents[processorIndex].product
+
+        items.motherboards = items.motherboards.filter(item => item.socket === processor.socket)
+        this.filters.Processors = processor.socket
+      } else {
+        delete this.filters.Processors
+      }
+
+      if (motherboardIndex !== -1) {
+        const motherboard = this.cartComponents[motherboardIndex].product
+
+        items.processors = items.processors.filter(item => item.socket === motherboard.socket)
+        items.ram = items.ram.filter(item => item.memoryType === motherboard.memoryType)
+        this.filters.Motherboard = motherboard.socket
+        this.filters.Ram = motherboard.memoryType
+      } else {
+        delete this.filters.Motherboard
+        delete this.filters.Ram
+      }
+
+
+      if (ramIndex !== -1) {
+        const ram = this.cartComponents[ramIndex].product
+
+        items.motherboards = items.motherboards.filter(item => item.memoryType === ram.memoryType)
+        this.filters.Ram = ram.memoryType
+      }
+
+      return items
+    }
+  },
+  async mounted () {
+    await this.loadData()
+  },
+  methods: {
+    async loadData () {
+      try {
+        this.items = (await import('../server/components')).default
+      } catch {
+        console.log('Load error')
+      }
+    },
+    addToCart (product) {
+      this.$store.dispatch('setProduct', {
+        product,
+        type: this.selectedComponent
+      })
+    }
+  }
+}
+</script>
+
+<template>
+  <v-row>
+    <div v-show="false">{{filteredItems}}</div>
+    <v-col cols="12" sm="12" md="4" lg="3" xl="3">
+      <v-card>
+        <v-list>
+          <v-subheader>Components</v-subheader>
+          <v-list-item-group v-model="selectedComponent" color="deep-purple accent-4">
+            <v-list-item
+              v-for="(item, i) in list"
+              :key="i"
+              :value="item.value"
+            >
+              <v-list-item-icon>
+                <v-icon v-text="item.icon"></v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title v-text="item.text"></v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+          <v-divider></v-divider>
+          <v-list-group
+            v-if="Object.keys(filters).length"
+            v-model="showFilters"
+            no-action
+            color="deep-purple accent-4"
+            prepend-icon="mdi-filter"
+          >
+            <template v-slot:activator>
+              <v-list-item-content>
+                <v-list-item-title>Filters</v-list-item-title>
+              </v-list-item-content>
+            </template>
+            <v-list-item class="pl-4" v-for="(filter, key) in filters">
+              <v-list-item-content>
+                <v-list-item-title>{{key}}</v-list-item-title>
+              </v-list-item-content>
+              <v-list-item-action>
+                <v-chip
+                  color="red"
+                  text-color="white"
+                >
+                  {{filter.toUpperCase()}}
+                </v-chip>
+                </v-list-item-action>
+            </v-list-item>
+          </v-list-group>
+        </v-list>
+      </v-card>
+    </v-col>
+    <v-col cols="12" sm="12" md="8" lg="9" xl="9" class="pt-2 product-container">
+      <v-row>
+        <v-col :key="item.id" v-for="item in filteredItems[selectedComponent]" cols="12" sm="12" md="6" lg="4" xl="3" class="pa-1">
+          <v-card height="435">
+            <v-img
+              :src="require('../' + item.image)"
+              height="200px"
+              :contain="true"
+            ></v-img>
+
+            <v-card-title>
+              {{item.name}}
+            </v-card-title>
+
+            <v-card-subtitle>
+              {{item.description}}
+            </v-card-subtitle>
+
+            <v-card-actions class="card-actions">
+          <span class="title font-weight-light pl-2">
+            {{item.price}} $
+          </span>
+
+              <v-spacer></v-spacer>
+              <v-btn
+                color="purple"
+                class="pb-2 pr-4"
+                icon
+                @click="addToCart(item)"
+              >
+                <v-icon large>mdi-cart</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-col>
+  </v-row>
+</template>
+
+<style scoped>
+  .card-actions {
+    position: absolute !important;
+    bottom: 0;
+    width: 100%;
+  }
+
+  .product-container {
+    height: calc(100vh - 80px);
+    overflow-x: auto;
+  }
+
+  .product-container::-webkit-scrollbar {
+    width: 3px;
+  }
+  .product-container::-webkit-scrollbar-thumb {
+    background: #f0f0f0;
+  }
+  .product-container::-webkit-scrollbar-thumb:hover {
+    background: rgba(86, 52, 233, 0.9);
+  }
+</style>
